94fd3e10ae323b907fac66363f13f1ae7a5b05e3
ocdman <ocdman2@gmail.com>
2014-09-04 17:00:42 +0800

    Start On User Defined Controls

---
+++ PcDesktop/Infrastructure/XIMALAYACOPY.Controls/Controls/MetroSlider.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace XIMALAYACOPY.Controls
+{
+    public class MetroSlider : Slider
+    {
+        /// <summary>
+        /// 滑块是否相反
+        /// </summary>
+        public bool IsReverse
+        {
+            get { return (bool)GetValue(IsReverseProperty); }
+            set { SetValue(IsReverseProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsReverse.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsReverseProperty =
+            DependencyProperty.Register("IsReverse", typeof(bool), typeof(MetroSlider), new PropertyMetadata(false));
+
+        /// <summary>
+        /// ThumbForeground
+        /// </summary>
+        public Brush ThumbForeground
+        {
+            get { return (Brush)GetValue(ThumbForegroundProperty); }
+            set { SetValue(ThumbForegroundProperty, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty ThumbForegroundProperty =
+            DependencyProperty.Register("ThumbForeground", typeof(Brush), typeof(MetroSlider), new PropertyMetadata(null));
+
+        /// <summary>
+        /// ThumbBackground
+        /// </summary>
+        public Brush ThumbBackground
+        {
+            get { return (Brush)GetValue(ThumbBackgroundProperty); }
+            set { SetValue(ThumbBackgroundProperty, value); }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty ThumbBackgroundProperty =
+            DependencyProperty.Register("ThumbBackground", typeof(Brush), typeof(MetroSlider), new PropertyMetadata(null));
+
+        /// <summary>
+        /// TrackBackgroundFront
+        /// </summary>
+        public Brush TrackBackgroundFront
+        {
+            get { return (Brush)GetValue(TrackBackgroundFrontProperty); }
+            set { SetValue(TrackBackgroundFrontProperty, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty TrackBackgroundFrontProperty =
+            DependencyProperty.Register("TrackBackgroundFront", typeof(Brush), typeof(MetroSlider), new PropertyMetadata(null));
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Brush TrackBackgroundBack
+        {
+            get { return (Brush)GetValue(TrackBackgroundBackProperty); }
+            set { SetValue(TrackBackgroundBackProperty, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty TrackBackgroundBackProperty =
+            DependencyProperty.Register("TrackBackgroundBack", typeof(Brush), typeof(MetroSlider), new PropertyMetadata(null));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Duration
+        {
+            get { return (double)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty DurationProperty =
+            DependencyProperty.Register("Duration", typeof(double), typeof(MetroSlider), new PropertyMetadata(0D));
+        /// <summary>
+        /// 
+        /// </summary>
+        public double TotalTimeSpan
+        {
+            get { return (double)GetValue(TotalTimeSpanProperty); }
+            set { SetValue(TotalTimeSpanProperty, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty TotalTimeSpanProperty =
+            DependencyProperty.Register("TotalTimeSpan", typeof(double), typeof(MetroSlider), new PropertyMetadata(0D, OnTimeTotalChanged));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double TotalBuffTimeSpan
+        {
+            get { return (double)GetValue(TotalBuffTimeSpanProperty); }
+            set { SetValue(TotalBuffTimeSpanProperty, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty TotalBuffTimeSpanProperty =
+            DependencyProperty.Register("TotalBuffTimeSpan", typeof(double), typeof(MetroSlider), new PropertyMetadata(0D));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public float Process
+        {
+            get { return (float)GetValue(ProcessProperty); }
+            set { SetValue(ProcessProperty, value); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty ProcessProperty =
+            DependencyProperty.Register("Process", typeof(float), typeof(MetroSlider), new PropertyMetadata(0F, OnProcessChanged));
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double BuffWidth
+        {
+            get { return (double)GetValue(BuffWidthProperty); }
+            set { SetValue(BuffWidthProperty, value); }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double CurrentWidth
+        {
+            get { return (double)GetValue(CurrentWidthProperty); }
+            set { SetValue(CurrentWidthProperty, value); }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty CurrentWidthProperty =
+            DependencyProperty.Register("CurrentWidth", typeof(double), typeof(MetroSlider), new PropertyMetadata(0D));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty BuffWidthProperty =
+            DependencyProperty.Register("BuffWidth", typeof(double), typeof(MetroSlider), new PropertyMetadata(0D));
+
+
+        public static void OnTimeTotalChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            MetroSlider slider = dependencyObject as MetroSlider;
+
+            if (slider.Duration > 0)
+            {
+                slider.IsReverse = slider.Value / slider.Duration > 0.5D;
+            }
+        }
+
+        public static void OnProcessChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            MetroSlider slider = dependencyObject as MetroSlider;
+
+            slider.BuffWidth = slider.ActualWidth * slider.Process;
+        }
+
+        public MetroSlider()
+        {
+            this.SizeChanged += MetroSlider_SizeChanged;
+            this.ValueChanged += MetroSlider_ValueChanged;
+        }
+
+        private void MetroSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.CurrentWidth = this.Value / this.Maximum * (this.ActualWidth - 10);
+        }
+
+        private void MetroSlider_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.BuffWidth = this.ActualWidth * this.Process;
+        }
+
+        static MetroSlider()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroSlider), new FrameworkPropertyMetadata(typeof(MetroSlider)));
+        }
+
+    }
+}
---
+++ PcDesktop/Infrastructure/XIMALAYACOPY.Controls/Controls/MyCheckboxButton.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XIMALAYACOPY.Controls
+{
+    /// <summary>
+    /// 带角标的toggle按钮
+    /// </summary>
+    [TemplatePart(Name = "PART_CheckBorderAnimation", Type = typeof(ThicknessAnimation))]
+    [TemplatePart(Name = "PART_BorderAnimation", Type = typeof(ThicknessAnimation))]
+    public class MyCheckboxButton : CheckBox
+    {
+        private ThicknessAnimation BorderAnimation { get; set; }
+        private ThicknessAnimation CheckBorderAnimation { get; set; }
+        /// <summary>
+        /// 角标的尺寸
+        /// </summary>
+        public double SuperScriptSize
+        {
+            get { return (double)GetValue(SuperScriptSizeProperty); }
+            set { SetValue(SuperScriptSizeProperty, value); }
+        }
+        /// <summary>
+        /// 角标的尺寸
+        /// </summary>
+        public static readonly DependencyProperty SuperScriptSizeProperty =
+            DependencyProperty.Register("SuperScriptSize", typeof(double), typeof(MyCheckboxButton), new PropertyMetadata(10D));
+        /// <summary>
+        /// 图标的path
+        /// </summary>
+        public Geometry IconData
+        {
+            get { return (Geometry)GetValue(IconDataProperty); }
+            set { SetValue(IconDataProperty, value); }
+        }
+        /// <summary>
+        /// 图标的path
+        /// </summary>
+        public static readonly DependencyProperty IconDataProperty =
+            DependencyProperty.Register("IconData", typeof(Geometry), typeof(MyCheckboxButton), new PropertyMetadata(null));
+        /// <summary>
+        /// 图表的宽度
+        /// </summary>
+        public double IconWidth
+        {
+            get { return (double)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+        /// <summary>
+        /// 图标的宽度
+        /// </summary>
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register("IconWidth", typeof(double), typeof(MyCheckboxButton), new PropertyMetadata(15D));
+        /// <summary>
+        /// 图标的高度
+        /// </summary>
+        public double IconHeight
+        {
+            get { return (double)GetValue(IconHeightProperty); }
+            set { SetValue(IconHeightProperty, value); }
+        }
+        /// <summary>
+        /// 图标的高度
+        /// </summary>
+        public static readonly DependencyProperty IconHeightProperty =
+            DependencyProperty.Register("IconHeight", typeof(double), typeof(MyCheckboxButton), new PropertyMetadata(15D));
+        /// <summary>
+        /// 图标margin值
+        /// </summary>
+        public Thickness IconMargin
+        {
+            get { return (Thickness)GetValue(IconMarginProperty); }
+            set { SetValue(IconMarginProperty, value); }
+        }
+        /// <summary>
+        /// 图标margin值
+        /// </summary>
+        public static readonly DependencyProperty IconMarginProperty =
+            DependencyProperty.Register("IconMargin", typeof(Thickness), typeof(MyCheckboxButton), new PropertyMetadata(new Thickness(5)));
+        /// <summary>
+        /// 图标的填充色
+        /// </summary>
+        public Brush IconFill
+        {
+            get { return (Brush)GetValue(IconFillProperty); }
+            set { SetValue(IconFillProperty, value); }
+        }
+        /// <summary>
+        /// 图标的填充色
+        /// </summary>
+        public static readonly DependencyProperty IconFillProperty =
+            DependencyProperty.Register("IconFill", typeof(Brush), typeof(MyCheckboxButton), new PropertyMetadata(new SolidColorBrush()));
+        /// <summary>
+        /// 选中后的边框尺寸
+        /// </summary>
+        public Thickness CheckedBorderThickness
+        {
+            get { return (Thickness)GetValue(CheckedBorderThicknessProperty); }
+            set { SetValue(CheckedBorderThicknessProperty, value); }
+        }
+        /// <summary>
+        /// 选中后的边框尺寸
+        /// </summary>
+        public static readonly DependencyProperty CheckedBorderThicknessProperty =
+            DependencyProperty.Register("CheckedBorderThickness", typeof(Thickness), typeof(MyCheckboxButton), new PropertyMetadata(new Thickness(1)));
+
+        /// <summary>
+        /// 应用模板
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this.BorderAnimation = GetTemplateChild("PART_BorderAnimation") as ThicknessAnimation;
+            this.CheckBorderAnimation = GetTemplateChild("PART_CheckBorderAnimation") as ThicknessAnimation;
+            this.BorderAnimation.To = this.BorderThickness;
+            this.CheckBorderAnimation.To = this.CheckedBorderThickness;
+        }
+
+        static MyCheckboxButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCheckboxButton), new FrameworkPropertyMetadata(typeof(MyCheckboxButton)));
+        }
+    }
+}
